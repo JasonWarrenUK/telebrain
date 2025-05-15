54114477e3c66f46226ac914a2d0315c5eeaf503
Jason Warren <contact.jwarren@gmail.com>
2025-05-15 12:18:18 +0100

    retrieve & echo username and message from prompt

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv";
 
 dotenv.config();
-
 console.log(process.env);
+
 const token = process.env.TELEGRAM_KEY || "";
 
 const bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/\/echo (.+)/, (msg/* :Message */, match) => {
-  const chatId = msg.chat.id;
+	const chatId = msg.chat.id;
 	
 	const resp = match ? match[1] : "NOTHIN'";
 
@@ -20,5 +20,29 @@ bot.onText(/\/echo (.+)/, (msg/* :Message */, match) => {
 bot.on('message', (msg/* :Message */) => {
   const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, 'uh... k');
+	const user = msg.from.first_name;
+	const text = msg.text;
+
+	const resp = `${user}, you said this: ${text}`
+
+  bot.sendMessage(chatId, resp);
 });
+
+/*
+{
+	"message_id":15,
+	"from":{
+		"id":7068960341,
+		"is_bot":false,
+		"first_name":"Jason",
+		"language_code":"en"
+	},
+	"chat":{
+		"id":7068960341,
+		"first_name":"Jason",
+		"type":"private"
+	},
+	"date":1747307686,
+	"text":"hi"
+}
+*/
